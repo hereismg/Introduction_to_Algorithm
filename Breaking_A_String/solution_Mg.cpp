d2f415b3af220dac4a69dbb7481c38d265cd7c6d
hereismg <hereismg@163.com>
2025-05-09 20:25:12 +0800

    update: append solution for 'Breaking_A_String'.

--- Breaking_A_String/solution_Mg.cpp
+++ Breaking_A_String/solution_Mg.cpp
@@ -2,16 +2,36 @@
 #include <iostream>
 using namespace std;
 
+pair<int, int> change(pair<int, int> coord){
+    int x = coord.first;
+    int y = coord.second;
+    return {y, x + y + 2};
+}
+
 int main(){
     int n, m;
     cin >> n >> m;
 
-    vector<int> split_arr(m);
-    for (int i=0; i<m; i++){
+    vector<int> split_arr(m + 2, 0);
+    split_arr[0] = 1;
+    for (int i=1; i<=m; i++){
         cin >> split_arr[i];
     }
+    split_arr[m+1] = n;
 
-    
+    vector<int> dp(m + 1, 0);
+
+    for (int i=0; i<m; i++){
+        for (int j=0; j<m-i; j++){
+            auto temp = change({i, j});
+            int x_p = temp.first;
+            int y_p = temp.second;
+            
+            dp[j] = min(dp[j], dp[j+1]) + split_arr[y_p] - split_arr[x_p] + 1;
+        }
+    }
+
+    cout << dp[0] << endl;;
 
     return 0;
 }
