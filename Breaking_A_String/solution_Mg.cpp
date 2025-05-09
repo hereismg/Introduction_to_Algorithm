281e8db0ba5e0d694c9abb5f30db15cf1a077b01
hereismg <hereismg@163.com>
2025-05-09 20:05:41 +0800

    bugfix: problem 'Breaking_A_String' consider is wrong.

--- Breaking_A_String/solution_Mg.cpp
+++ Breaking_A_String/solution_Mg.cpp
@@ -1,7 +1,17 @@
+#include <vector>
 #include <iostream>
 using namespace std;
 
 int main(){
+    int n, m;
+    cin >> n >> m;
+
+    vector<int> split_arr(m);
+    for (int i=0; i<m; i++){
+        cin >> split_arr[i];
+    }
+
     
+
     return 0;
 }
